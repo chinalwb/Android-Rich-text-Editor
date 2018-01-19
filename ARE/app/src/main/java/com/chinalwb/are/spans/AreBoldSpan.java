f542c7ea6ec7878468474996dbf99cb01f689f96
Rain Liu <liuwenbin168@126.com>
2018-01-20 00:38:28 +0800

    Check in missing files

---
+++ ARE/app/src/main/java/com/chinalwb/are/spans/AreBoldSpan.java
@@ -0,0 +1,15 @@
+package com.chinalwb.are.spans;
+
+import android.graphics.Typeface;
+import android.text.style.StyleSpan;
+
+/**
+ * Created by wliu on 2018/1/19.
+ */
+
+public class AreBoldSpan extends StyleSpan {
+
+    public AreBoldSpan() {
+        super(Typeface.BOLD);
+    }
+}
---
+++ ARE/app/src/main/java/com/chinalwb/are/styles/ARE_ABS_FreeStyle.java
@@ -0,0 +1,32 @@
+package com.chinalwb.are.styles;
+
+import android.content.Context;
+import android.text.Editable;
+import android.text.Spanned;
+import android.widget.EditText;
+
+import com.chinalwb.are.Util;
+import com.chinalwb.are.styles.toolbar.ARE_Toolbar;
+
+import java.lang.reflect.ParameterizedType;
+
+public abstract class ARE_ABS_FreeStyle implements IARE_Style {
+
+	protected Context mContext;
+
+	public ARE_ABS_FreeStyle() {
+		this.mContext = ARE_Toolbar.getInstance().getContext();
+	}
+	
+	@Override
+	public EditText getEditText() {
+		return ARE_Toolbar.getInstance().getEditText();
+	}
+
+
+	// Dummy implementation
+	@Override
+	public boolean getIsChecked() {
+		return false;
+	}
+}
