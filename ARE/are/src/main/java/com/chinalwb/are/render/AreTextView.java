ee95b0d742153dcce091a7c3a571da3cdb8d61a1
Rain Liu <liuwenbin168@126.com>
2018-07-10 16:27:44 +0800

    Added cache for being used in ListView

--- ARE/are/src/main/java/com/chinalwb/are/render/AreTextView.java
+++ ARE/are/src/main/java/com/chinalwb/are/render/AreTextView.java
@@ -14,6 +14,8 @@ import com.chinalwb.are.events.AREMovementMethod;
 import com.chinalwb.are.strategies.AreClickStrategy;
 import com.chinalwb.are.strategies.defaults.DefaultClickStrategy;
 
+import java.util.HashMap;
+
 /**
  * @author dlink
  * @email linxy59@mail2.sysu.edu.cn
@@ -22,6 +24,9 @@ import com.chinalwb.are.strategies.defaults.DefaultClickStrategy;
  * @usage null
  */
 public class AreTextView extends AppCompatTextView {
+
+    private static HashMap<String, Spanned> spannedHashMap = new HashMap<>();
+
     private AreClickStrategy mClickStrategy;
 
     Context mContext;
@@ -56,11 +61,39 @@ public class AreTextView extends AppCompatTextView {
     }
 
     public void fromHtml(String html) {
+        Spanned spanned = getSpanned(html);
+        setText(spanned);
+    }
+
+    private Spanned getSpanned(String html) {
         Html.sContext = mContext;
         Html.ImageGetter imageGetter = new AreImageGetter(mContext, this);
         Html.TagHandler tagHandler = new AreTagHandler();
-        Spanned spanned = Html.fromHtml(html, Html.FROM_HTML_SEPARATOR_LINE_BREAK_PARAGRAPH, imageGetter, tagHandler);
-        setText(spanned);
+        return Html.fromHtml(html, Html.FROM_HTML_SEPARATOR_LINE_BREAK_PARAGRAPH, imageGetter, tagHandler);
+    }
+
+    /**
+     * Use cache will take more RAM, you need to call clear cache when you think it is safe to do that.
+     * You may need cache when working with {@link android.widget.ListView} or RecyclerView
+     *
+     * @param html
+     */
+    public void fromHtmlWithCache(String html) {
+        Spanned spanned = null;
+        if (spannedHashMap.containsKey(html)) {
+            spanned = spannedHashMap.get(html);
+        }
+        if (spanned == null) {
+            spanned = getSpanned(html);
+            spannedHashMap.put(html, spanned);
+        }
+        if (spanned != null) {
+            setText(spanned);
+        }
+    }
+
+    public static void clearCache() {
+        spannedHashMap.clear();
     }
 
     public void setClickStrategy(AreClickStrategy clickStrategy) {
