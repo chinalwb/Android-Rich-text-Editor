b6dcdbbee09224d79103452c5be6afd9cb3e2b46
Rain Liu <liuwenbin168@126.com>
2018-08-10 17:14:24 +0800

    Init AREditor dynamically

--- ARE/are/src/main/java/com/chinalwb/are/AREditor.java
+++ ARE/are/src/main/java/com/chinalwb/are/AREditor.java
@@ -3,21 +3,26 @@ package com.chinalwb.are;
 import android.animation.ObjectAnimator;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.text.Editable;
-import android.text.Spannable;
+import android.text.Layout;
 import android.text.Spanned;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
+import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.ScrollView;
 
 import com.chinalwb.are.android.inner.Html;
 import com.chinalwb.are.render.AreImageGetter;
 import com.chinalwb.are.render.AreTagHandler;
-import com.chinalwb.are.spans.AreListSpan;
 import com.chinalwb.are.strategies.AtStrategy;
 import com.chinalwb.are.strategies.VideoStrategy;
 import com.chinalwb.are.styles.toolbar.ARE_Toolbar;
+
+import org.w3c.dom.Attr;
 
 /**
  * All Rights Reserved.
@@ -26,6 +31,21 @@ import com.chinalwb.are.styles.toolbar.ARE_Toolbar;
  *
  */
 public class AREditor extends RelativeLayout {
+
+	/**
+	 * The toolbar alignment.
+	 */
+	public enum ToolbarAlignment {
+		/**
+		 * (Default) Below input area.
+		 */
+		BOTTOM,
+
+		/**
+		 * Above the input area.
+		 */
+		TOP,
+	}
 
 	/*
 	 * --------------------------------------------
@@ -44,9 +64,19 @@ public class AREditor extends RelativeLayout {
 	private ARE_Toolbar mToolbar;
 
 	/**
+	 * The scroll view out of AREditText.
+	 */
+	private ScrollView mAreScrollView;
+
+	/**
 	 * The are editor.
 	 */
 	private AREditText mAre;
+
+	/**
+	 * The alignment of toolbar.
+	 */
+	private ToolbarAlignment mToolbarAlignment = ToolbarAlignment.TOP;
 
 	/*
 	 * --------------------------------------------
@@ -95,27 +125,42 @@ public class AREditor extends RelativeLayout {
 	 * Initialization.
 	 */
 	private void init() {
-		LayoutInflater layoutInflater = LayoutInflater.from(this.mContext);
-		if (null != layoutInflater) {
-			layoutInflater.inflate(getLayoutId(), this, true);
-		} else {
-			return;
-		}
-
-		this.initViews();
+		initSelf();
+		addToolbar();
+		addEditText();
 	} // # End of init()
 
-	private int getLayoutId() {
-		return R.layout.areditor;
+	private void initSelf() {
+		LayoutParams rootLayoutParams = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+		this.setLayoutParams(rootLayoutParams);
 	}
 
-	/**
-	 * Init views.
-	 */
-	private void initViews() {
-		this.mToolbar = this.findViewById(R.id.toolbar);
-		this.mAre = this.findViewById(R.id.are);
+	private void addToolbar() {
+		int ruleVerb = mToolbarAlignment == ToolbarAlignment.BOTTOM ? RelativeLayout.ALIGN_PARENT_BOTTOM : RelativeLayout.ALIGN_PARENT_TOP;
+		this.mToolbar = new ARE_Toolbar(mContext);
 		this.mToolbar.setEditText(mAre);
+		this.mToolbar.setId(R.id.are_toolbar);
+		LayoutParams layoutParams = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
+		layoutParams.addRule(ruleVerb, this.getId());
+		this.addView(this.mToolbar, layoutParams);
+	}
+
+	private void addEditText() {
+		int ruleVerb = mToolbarAlignment == ToolbarAlignment.BOTTOM ? RelativeLayout.ABOVE : RelativeLayout.BELOW;
+		mAreScrollView = new ScrollView(mContext);
+		LayoutParams scrollViewLayoutParams = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+		scrollViewLayoutParams.addRule(ruleVerb, mToolbar.getId());
+
+		mAre = new AREditText(mContext);
+		LayoutParams editTextLayoutParams = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+		mAreScrollView.addView(mAre, editTextLayoutParams);
+
+		this.addView(mAreScrollView, scrollViewLayoutParams);
+	}
+
+	private void relayoutToolbar() {
+//		int ruleVerb = mToolbarAlignment == ToolbarAlignment.BOTTOM ? RelativeLayout.ALIGN_PARENT_BOTTOM : RelativeLayout.ALIGN_PARENT_TOP;
+//		((RelativeLayout.LayoutParams) mToolbar.getLayoutParams()).getRules();
 	}
 
 	/**
@@ -125,25 +170,25 @@ public class AREditor extends RelativeLayout {
 	 * @return
 	 */
 	public void fromHtml(String html) {
-	    Html.sContext = mContext;
-        Html.ImageGetter imageGetter = new AreImageGetter(mContext, this.mAre);
-        Html.TagHandler tagHandler = new AreTagHandler();
-        Spanned spanned = Html.fromHtml(html, Html.FROM_HTML_SEPARATOR_LINE_BREAK_PARAGRAPH, imageGetter, tagHandler);
-        AREditText.stopMonitor();
-        this.mAre.getEditableText().append(spanned);
-        AREditText.startMonitor();
-        log();
-    }
+		Html.sContext = mContext;
+		Html.ImageGetter imageGetter = new AreImageGetter(mContext, this.mAre);
+		Html.TagHandler tagHandler = new AreTagHandler();
+		Spanned spanned = Html.fromHtml(html, Html.FROM_HTML_SEPARATOR_LINE_BREAK_PARAGRAPH, imageGetter, tagHandler);
+		AREditText.stopMonitor();
+		this.mAre.getEditableText().append(spanned);
+		AREditText.startMonitor();
+//        log();
+	}
 
 	private void log() {
-        Editable editable = this.mAre.getEditableText();
-        Object[] spans = editable.getSpans(0, editable.length(), Object.class);
-        for (Object span : spans) {
-            int spanStart = editable.getSpanStart(span);
-            int spanEnd = editable.getSpanEnd(span);
-	        Util.log("span == " + span + ", start == " + spanStart + ", end == " + spanEnd);
-        }
-    }
+		Editable editable = this.mAre.getEditableText();
+		Object[] spans = editable.getSpans(0, editable.length(), Object.class);
+		for (Object span : spans) {
+			int spanStart = editable.getSpanStart(span);
+			int spanEnd = editable.getSpanEnd(span);
+			Util.log("span == " + span + ", start == " + spanStart + ", end == " + spanEnd);
+		}
+	}
 	/**
 	 *
 	 */
@@ -176,16 +221,29 @@ public class AREditor extends RelativeLayout {
 		this.mToolbar.onActivityResult(requestCode, resultCode, data);
 	} // #End of onActivityResult(..)
 
-
 	/* ----------------------
 	 * Customization part
 	 * ---------------------- */
+
+	public void hideToolbar() {
+
+	}
+
+	public void showToolbar() {
+
+	}
+
+	public void setToolbarAlignment(ToolbarAlignment alignment) {
+		mToolbarAlignment = alignment;
+		relayoutToolbar();
+	}
+
 	public void setAtStrategy(AtStrategy atStrategy) {
 		this.mAre.setAtStrategy(atStrategy);
 	}
 
 	public void setVideoStrategy(VideoStrategy videoStrategy) {
-        this.mAre.setVideoStrategy(videoStrategy);
+		this.mAre.setVideoStrategy(videoStrategy);
 	}
 
 }
